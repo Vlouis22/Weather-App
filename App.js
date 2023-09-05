002d19a28b70a4c6cd66a1af3c4f1f9592b7ec97
Vlouis22 <116316770+Vlouis22@users.noreply.github.com>
2023-09-05 12:20:51 -0400

    Add files via upload
    Updated

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WeatherApp from './weather';
+
+function App() {
+  // Here we're using the useState hook to create some state for our counter
+
+  return (
+    <div>
+     <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
